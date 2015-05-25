895927670a30a99fce1312e5f437c1e8c2c61c38
VonC <vonc@laposte.net>
2015-05-25 18:04:50 +0200

    seec.go: search user by name if login is not provided

--- seec.go
+++ seec.go
@@ -23,14 +23,14 @@ func main() {
 	if len(commit.Parents) != 2 {
 		fmt.Printf("Sha1 '%s' has '%d' parent(s) instead of 2\n", sha1, len(commit.Parents))
 	}
-	clogin := login(*commit.Author.Email, client)
+	clogin := login(*commit.Author.Email, *commit.Author.Name, client)
 	parent := commit.Parents[1]
 	pcommit, _, err := client.Git.GetCommit("git", "git", *parent.SHA)
 	if err != nil {
 		fmt.Printf("Unable to get parent commit '%s': err '%v'\n", parent.SHA, err)
 		os.Exit(1)
 	}
-	plogin := login(*pcommit.Author.Email, client)
+	plogin := login(*pcommit.Author.Email, *pcommit.Author.Name, client)
 
 	res := fmt.Sprintf("See [commit %s](https://github.com/git/git/commit/%s) by [%s (`%s`)](https://github.com/%s), %s.  \n",
 		(*pcommit.SHA)[:7], *pcommit.SHA,
@@ -43,15 +43,23 @@ func main() {
 	fmt.Println("(Copied to the cipboard)")
 }
 
-func login(email string, client *github.Client) string {
+func login(email string, name string, client *github.Client) string {
 	opts := &github.SearchOptions{Order: "desc"}
-	res, _, err := client.Search.Users(email, opts)
-	if err != nil {
-		fmt.Printf("Unable to search user '%s': err '%v'", email, err)
-		os.Exit(1)
+	var res *github.UsersSearchResult
+	var err error
+	if email != "" {
+		res, _, err = client.Search.Users(email, opts)
+		if err != nil {
+			fmt.Printf("Unable to search user '%s': err '%v'", email, err)
+			os.Exit(1)
+		}
 	}
-	if *res.Total == 0 {
-		return ""
+	if res == nil || *res.Total == 0 {
+		res, _, err = client.Search.Users(name, opts)
+		if err != nil {
+			fmt.Printf("Unable to search user '%s': err '%v'", name, err)
+			os.Exit(1)
+		}
 	}
 	return *res.Users[0].Login
 }
