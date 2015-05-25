5519f22033519c70953ae7b70387cc7f3a416209
VonC <vonc@laposte.net>
2015-05-25 18:03:43 +0200

    gb.bat: add dependency to global GOPATH if not found

--- gb.bat
+++ gb.bat
@@ -1,4 +1,12 @@
 @echo off
+set go-github=src\github.com\google\go-github
+if not "%GOPATH%" == "" (
+	if not exist %GOPATH%\%go-github% (
+		mklink /D %~dp0\deps\%go-github% %GOPATH%\%go-github%
+		cd %GOPATH%\%go-github%
+		go install
+	)
+)
 setlocal
 set GOPATH=%~dp0/deps
 set GOBIN=%~dp0/bin
