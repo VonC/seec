77a923f109705b142a1e9b50caf8b6eafb1a6915
VonC <vonc@laposte.net>
2015-06-08 21:53:04 +0200

    seec.go: add pdbg, for debug messages, if env(dbg) is set

--- seec.go
+++ seec.go
@@ -6,6 +6,7 @@ import (
 	"regexp"
 	"strings"
 
+	"github.com/VonC/godbg"
 	"github.com/VonC/godbg/exit"
 	"github.com/atotto/clipboard"
 	"github.com/google/go-github/github"
@@ -13,14 +14,21 @@ import (
 
 var client *github.Client
 var ex *exit.Exit
+var pdbg *godbg.Pdbg
 
 func init() {
 	ex = exit.Default()
+	if os.Getenv("dbg") != "" {
+		pdbg = godbg.NewPdbg()
+	} else {
+		pdbg = godbg.NewPdbg(godbg.OptExcludes([]string{"/seec.go"}))
+	}
 }
 
 func main() {
 	if len(os.Args) != 2 {
-		fmt.Println("Usage: go run seec.go <sha1>")
+		fmt.Println("Usage:       go run seec.go <sha1>")
+		fmt.Println("       dbg=1 go run seec.go <sha1> for debug information")
 		ex.Exit(0)
 	}
 	sha1 := os.Args[1]
