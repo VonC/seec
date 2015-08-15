241b4d6fce53fb141dd5d585e52d0ece60e95729
VonC <vonc@laposte.net>
2015-08-15 16:51:34 +0200

    seec.go: logins.txt is cache for GitHub logins

--- seec.go
+++ seec.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
 	"os"
 	"regexp"
 	"strings"
@@ -150,7 +155,7 @@ func seeCommit(parent, commit *github.Commit) string {
 		author := pcommitsByAuthor.author
 		commitsbd := pcommitsByAuthor.cbd
 		first := true
-		plogin := login(*author.Email, *author.Name)
+		plogin := login(*author.Email, *author.Name, *commitsbd[0].commits[0].SHA)
 		for i, cbd := range commitsbd {
 			if first {
 				res = res + "See "
@@ -250,7 +255,14 @@ func (cba *commitsByAuthor) addCba(acba *commitsByAuthor) {
 	}
 }
 
-func login(email string, name string) string {
+func login(email string, name string, SHA string) string {
+	fmt.Printf("email='%s', name='%s'\n", email, name)
+	if login := cacheLogins(email, name); login != "" {
+		return login
+	}
+	if login := scrapPage(SHA); login != "" {
+		return addToCacheLogins(email, name, login)
+	}
 	opts := &github.SearchOptions{Order: "desc"}
 	var res *github.UsersSearchResult
 	var err error
@@ -281,12 +293,12 @@ func login(email string, name string) string {
 			return ""
 		}
 		issue := resIssues.Issues[0]
-		return *issue.User.Login
+		return addToCacheLogins(email, name, *issue.User.Login)
 	}
 	if res == nil || *res.Total == 0 {
 		return ""
 	}
-	return *res.Users[0].Login
+	return addToCacheLogins(email, name, *res.Users[0].Login)
 }
 
 func collect(res, msg, activity string) string {
@@ -301,7 +313,7 @@ func collect(res, msg, activity string) string {
 		}
 		name := resc[1]
 		email := resc[2]
-		login := login(email, name)
+		login := login(email, name, "")
 		if !first {
 			activitymsg = activitymsg + ", "
 		}
@@ -323,4 +335,105 @@ func collect(res, msg, activity string) string {
 		res = res + activitymsg + "  \n"
 	}
 	return res
+}
+
+var cacheLoginsRE = regexp.MustCompile(`^(.*)#~#(.*?)\^\((.*?)\)\^(.*?)$`)
+
+func cacheLogins(email string, name string) string {
+	login := ""
+	fi, err := os.OpenFile("logins.txt", os.O_RDONLY|os.O_CREATE, 0660)
+	if err != nil {
+		panic(err)
+	}
+	// close fi on exit and check for its returned error
+	defer func() {
+		if err := fi.Close(); err != nil {
+			panic(err)
+		}
+	}()
+	var part []byte
+	var prefix bool
+	reader := bufio.NewReader(fi)
+	buffer := bytes.NewBuffer(make([]byte, 0))
+	for {
+		if part, prefix, err = reader.ReadLine(); err != nil {
+			break
+		}
+		buffer.Write(part)
+		if !prefix {
+			line := buffer.String()
+			re := cacheLoginsRE.FindAllStringSubmatch(line, -1)
+			if len(re) == 1 {
+				femail := re[0][1]
+				fname := re[0][2]
+				flogin := re[0][4]
+				if femail == email && fname == name {
+					// fmt.Printf("femail='%s', fname='%s' => login '%s'\n", femail, fname, flogin)
+					login = flogin
+					break
+				}
+			}
+			buffer.Reset()
+		}
+	}
+	if err == io.EOF {
+		err = nil
+	}
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("cacheLogins='%s'\n", login)
+	return login
+}
+
+var contributorRE = regexp.MustCompile(`.*?<a\s+href="[^"]+"\s+rel="contributor"\s*?>([^<]+)</a>`)
+
+func scrapPage(SHA string) string {
+	if SHA == "" {
+		return ""
+	}
+	response, err := http.Get("https://github.com/git/git/commit/" + SHA)
+	if err != nil {
+		fmt.Printf("%s", err)
+		os.Exit(1)
+	} else {
+		defer response.Body.Close()
+		contents, err := ioutil.ReadAll(response.Body)
+		if err != nil {
+			fmt.Printf("%s", err)
+			os.Exit(1)
+		}
+		lines := strings.Split(string(contents), `\n`)
+		for _, line := range lines {
+			if strings.Contains(line, "contributor") {
+				r := contributorRE.FindAllStringSubmatch(line, -1)
+				if len(r) > 0 {
+					if len(r[0]) == 2 {
+						// fmt.Printf("r[0]='%+v' => res='%s'\n", r[0], r[0][1])
+						return r[0][1]
+					}
+				}
+				fmt.Println("line='%+v'\n", r)
+			}
+		}
+	}
+	return ""
+}
+func addToCacheLogins(email string, name string, login string) string {
+	fmt.Printf("addToCacheLogins '%s'\n", login)
+	fi, err := os.OpenFile("logins.txt", os.O_RDWR|os.O_APPEND|os.O_CREATE, 0660)
+	if err != nil {
+		panic(err)
+	}
+	// close fi on exit and check for its returned error
+	defer func() {
+		if err := fi.Close(); err != nil {
+			panic(err)
+		}
+	}()
+	if _, err := fi.WriteString(fmt.Sprintf("%s#~#%s^()^%s\n", email, name, login)); err != nil {
+		panic(err)
+	}
+	return login
+
 }
