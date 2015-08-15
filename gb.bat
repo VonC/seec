1d4a4e1baa7073d87a713880578095c301c232f0
VonC <vonc@laposte.net>
2015-08-15 10:56:30 +0200

    gb.bat: uses MKLINK /J to avoid "not enough privilege" error

--- gb.bat
+++ gb.bat
@@ -1,16 +1,20 @@
 @echo off
 set go-github=src\github.com\google\go-github
 if not "%GOPATH%" == "" (
-	if not exist %GOPATH%\%go-github% (
-		mklink /D %~dp0\deps\%go-github% %GOPATH%\%go-github%
-		cd %GOPATH%\%go-github%
+	if not exist "%GOPATH%\%go-github%" (
+		mklink /J "%GOPATH%\%go-github%" "%~dp0\deps\%go-github%"
+		cd "%GOPATH%\%go-github%\github"
 		go install
 	)
 )
 setlocal
+set src=src\github.com\VonC\godbg
+if not exist "%~dp0\deps\%src%" (
+	mklink /J "%~dp0\deps\%src%" "%~dp0"
+)
 set GOPATH=%~dp0/deps
 set GOBIN=%~dp0/bin
-cd %~dp0
+cd "%~dp0"
 go install
 endlocal
 doskey seec=%~dp0\bin\seec.exe $*
