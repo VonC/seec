f50c164a7fba3cc433417e2d1bd14737872e658c
VonC <vonc@laposte.net>
2016-02-27 15:04:15 +0100

    gb.bat: use vendor, create src symlink

--- gb.bat
+++ gb.bat
@@ -1,20 +1,15 @@
 @echo off
-set go-github=src\github.com\google\go-github
-if not "%GOPATH%" == "" (
-	if not exist "%GOPATH%\%go-github%" (
-		mklink /J "%GOPATH%\%go-github%" "%~dp0\deps\%go-github%"
-		cd "%GOPATH%\%go-github%\github"
-		go install
-	)
-)
 setlocal
-set src=src\github.com\VonC\godbg
-if not exist "%~dp0\deps\%src%" (
-	mklink /J "%~dp0\deps\%src%" "%~dp0"
+set GOPATH=%~dp0
+if not exist src\github.com\VonC (
+	mkdir src\github.com\VonC
 )
-set GOPATH=%~dp0/deps
-set GOBIN=%~dp0/bin
-cd "%~dp0"
-call go install
+if not exist src\github.com\VonC\seec (
+	mklink /J src\github.com\VonC\seec %GOPATH%
+)
+pushd %~dp0
+cd src\github.com\VonC\seec
+cd
+go install
+popd
 endlocal
-echo doskey seec=%~dp0bin\seec.exe $*
