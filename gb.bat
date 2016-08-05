39e2d064cafc3fa1d72f1a717101b27a76874f61
VonC <vonc@laposte.net>
2016-08-05 15:41:20 +0200

    uses seec2

--- gb.bat
+++ gb.bat
@@ -4,11 +4,11 @@ set GOPATH=%~dp0
 if not exist src\github.com\VonC (
 	mkdir src\github.com\VonC
 )
-if not exist src\github.com\VonC\seec (
-	mklink /J src\github.com\VonC\seec %GOPATH%
+if not exist src\github.com\VonC\seec2 (
+	mklink /J src\github.com\VonC\seec2 %GOPATH%
 )
 pushd %~dp0
-cd src\github.com\VonC\seec
+cd src\github.com\VonC\seec2
 cd
 go install
 popd
